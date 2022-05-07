e0e5b8676054918a32e77a77ee7924427b6f0ebb
Shane Williams <shane@shanewilliams.ca>
2022-05-07 10:26:10 -0700

    Remove about page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -30,14 +30,14 @@ const routes = [
     name: 'radio',
     component: RadioView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // },
   {
     path: '/:catchAll(.*)*',
     name: "PageNotFound",
