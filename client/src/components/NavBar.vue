55ffe040243bd4072dab937a4f38ed2c24cc9d69
shanewilliams29 <shanewilliams29@gmail.com>
2022-05-18 18:28:35 -0700

    Sidebar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <div class="container-fluid">
     <b-navbar type="dark" variant="dark">
       <b-navbar-brand href="#">
@@ -12,17 +13,55 @@
         </b-nav>
       </div>
       <b-navbar-nav class="ml-auto" v-if="!$auth.clientToken">
-        <b-nav-item disabled right class="log-in-with">Log in </b-nav-item>
+<!--         <b-nav-item disabled right class="log-in-with">Log in </b-nav-item> -->
         <b-button right variant="success" class="spotify-button" :href="spotifyURL">
           <img :src="spotifyLogoURL" class="" alt="Spotify" height="28px" />
         </b-button>
+        <b-nav-item class="menu-button" right v-b-toggle.sidebar-right><b-icon-three-dots-vertical></b-icon-three-dots-vertical></b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto" v-if="$auth.clientToken">
-        <b-nav-item href="/log_out" right>Log out</b-nav-item>
-        <b-avatar :src="'https://storage.googleapis.com/composer-explorer.appspot.com/avatars/' + $auth.userid + '.jpg'"></b-avatar>
+        <b-avatar href="/log_out" :src="'https://storage.googleapis.com/composer-explorer.appspot.com/avatars/' + $auth.userid + '.jpg'"></b-avatar>
+              <b-nav-item class="menu-button" right v-b-toggle.sidebar-right><b-icon-three-dots-vertical></b-icon-three-dots-vertical></b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
+  <div>
+      <b-sidebar 
+        id="sidebar-right" 
+        title=""
+        backdrop-variant="dark"
+        width="350px"
+        backdrop
+        right 
+        shadow>
+      <div class="px-3 py-0 sidebar-text">
+        <h6>Composer Explorer</h6>
+        <p>Welcome to your Classical Music Portal. Explore composers from the Medieval to the present.
+Listen to works on Spotify, made navigable for Classical music.</p>
+        <h6>Usage</h6>
+        <p>
+          Log in with your <b>Spotify Premium</b> account to play music. Add performances to your favorites and create your own customized radios.
+        </p>
+        <h6>Acknowledgements</h6>
+        <p>
+          Composer and work information are used under licence from <a href="https://en.wikipedia.org/" target="_blank">Wikipedia</a>, <a href="https://imslp.org/" target="_blank">IMSLP</a>, and <a href="https://openopus.org/" target="_blank">Open Opus</a>. Album data and cover art provided by <a href="https://www.spotify.com/" target="_blank">Spotify</a>. 
+        </p>
+        <h6>Disclaimer</h6>
+         <p>
+          ComposerExplorer.com is not associated with Spotify Technology SA. Artist, album and track records are retrieved through the Spotify search API and do not necessarily represent the full extent of the Spotify catalogue for a given work. No guarantee is made as to the accuracy of catalogued composer and work information, and use is recommended for recreational purposes only.
+        </p>
+          <h6>Contact</h6>
+         <p>
+         For all inquiries, please contact: <a href="mailto:admin@composerexplorer.com">admin@composerexplorer.com</a>
+        </p>
+        <h6>Copyright</h6>
+          <p>
+          © 2022 ComposerExplorer.com. All rights reserved.
+        </p>
+      </div>
+    </b-sidebar>
+  </div>
+</div>
 </template>
 
 <script>
@@ -74,5 +113,11 @@ img {
 }
 .navbar-items a {
   color: white !important;
+}
+.menu-button a {
+  padding-right: 0px !important;
+}
+.sidebar-text{
+  font-size: 14px;
 }
 </style>
