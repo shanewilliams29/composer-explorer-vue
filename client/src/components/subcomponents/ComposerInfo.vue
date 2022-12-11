362a4d057dbe380522e41ef8afaaee80944464b3
shanewilliams29 <shanewilliams29@gmail.com>
2022-12-11 14:10:39 -0800

    Refactor code

--- client/src/components/subcomponents/ComposerInfo.vue
+++ client/src/components/subcomponents/ComposerInfo.vue
@@ -61,25 +61,27 @@ export default {
 
       var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-      if(isSafari){ // Cant's use lookbehind regex in Safari
+      if (isSafari) { // Cant's use lookbehind regex in Safari
         return unbrokenText;
+      
       } else {
         // Split the text into an array of sentences
         let regex = "/(?<![A-Z])(?<!No)(?<!Op)[.]/"
         let sentences = unbrokenText.split(eval(regex));
-          // Initialize the result to the first sentence
-          let result = sentences[0]
+        
+        // Initialize the result to the first sentence
+        let result = sentences[0]
       
-          // Loop through the remaining sentences, adding line breaks every 3 sentences
-          for (let i = 1; i < sentences.length - 1; i++) {
-            if (i % 2 === 0) {
-              result += ".\n\n";
-            } else {
-              result += ". ";
-            }
-            result += sentences[i];
+        // Loop through the remaining sentences, adding line breaks every 2 sentences
+        for (let i = 1; i < sentences.length - 1; i++) {
+          if (i % 2 === 0) {
+            result += ".\n\n";
+          } else {
+            result += ". ";
           }
-          return result + ".";
+          result += sentences[i];
+        }
+        return result + ".";
       }
     },
     getComposerInfo(composer) {
--- client/src/components/subcomponents/WorkInfo.vue
+++ client/src/components/subcomponents/WorkInfo.vue
@@ -65,23 +65,13 @@ export default {
     },
   },
   methods: {
-    checkIfWorkMatches(opus, title, text){
+    checkIfWorkMatches(opus, title, text) {
       if (opus) {
-        var num = opus.match(/\d+/);
-
-        let regex = new RegExp(`\\b${num}\\w?`);
-
-        if (text.search(regex) !== -1) {
-          return true;
-        } else {
-          return false;
-        }
+        const num = opus.match(/\d+/);
+        const regex = new RegExp(`\\b${num}\\w?`);
+        return regex.test(text);
       } else {
-        if (text.toLowerCase().includes(title.toLowerCase())) {
-          return true;
-        } else {
-          return false;
-        }
+        return text.toLowerCase().includes(title.toLowerCase());
       }
     },
     addLineBreaksToParagraph(paragraph){
@@ -90,25 +80,27 @@ export default {
 
       var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-      if(isSafari){ // Cant's use lookbehind regex in Safari
+      if (isSafari) { // Cant's use lookbehind regex in Safari
         return unbrokenText;
+      
       } else {
-        // Split the text into an array of sentences (?<![A-Z])(?<!No)(?<!Op)\b[.](?![.])
+        // Split the text into an array of sentences
         let regex = "/(?<![A-Z])(?<!No)(?<!Op)[.]/"
         let sentences = unbrokenText.split(eval(regex));
-          // Initialize the result to the first sentence
-          let result = sentences[0]
+        
+        // Initialize the result to the first sentence
+        let result = sentences[0]
       
-          // Loop through the remaining sentences, adding line breaks every 3 sentences
-          for (let i = 1; i < sentences.length - 1; i++) {
-            if (i % 2 === 0) {
-              result += ".\n\n";
-            } else {
-              result += ". ";
-            }
-            result += sentences[i];
+        // Loop through the remaining sentences, adding line breaks every 2 sentences
+        for (let i = 1; i < sentences.length - 1; i++) {
+          if (i % 2 === 0) {
+            result += ".\n\n";
+          } else {
+            result += ". ";
           }
-          return result + ".";
+          result += sentences[i];
+        }
+        return result + ".";
       }
     },
     getWorkInfo(work) {
