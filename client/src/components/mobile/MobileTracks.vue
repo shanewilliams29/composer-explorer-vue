05c85bae593d274ce760a1d4391fc703e293973f
shanewilliams29 <shanewilliams29@gmail.com>
2023-04-04 10:41:00 -0700

    Bug fix for mobile playback

--- client/src/components/mobile/MobileTracks.vue
+++ client/src/components/mobile/MobileTracks.vue
@@ -41,6 +41,7 @@
 <script>
 import { eventBus } from "@/main.js";
 import { trackMixin } from "@/components/playback/TrackListing.js"
+import axios from 'axios';
 
 export default {
   mixins: [trackMixin],
@@ -72,6 +73,46 @@ export default {
         window.firstLoad = false;
       }
     })
+    eventBus.$on("fireSetAlbumHopper", (album, track_no, percent_progress) => {
+      
+      this.$view.percentProgress = percent_progress;
+      this.genre = this.$config.genre;
+      this.$config.allTracks = album.tracks[0][2];
+      
+      try{
+        this.$config.playTracks = album.tracks[track_no][2];
+      } catch (error){
+        this.$config.playTracks = album.tracks[0][2];
+      }
+      localStorage.setItem("config", JSON.stringify(this.$config));
+      this.album = album;
+
+      let playTrack = this.$config.playTracks.split(' ')[0].replace('spotify:track:', '');
+
+      if (this.$auth.clientToken && this.$auth.deviceID && !window.firstLoad) {
+        const path = 'https://api.spotify.com/v1/tracks/' + playTrack;
+        axios({
+          method: 'get',
+          url: path,
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + this.$auth.clientToken
+          }
+        }).then((res) => {
+          let duration = res.data.duration_ms;
+          let position = Math.round(duration * this.$view.percentProgress - 3000);
+          if (position < 0){
+            position = 0;
+          }
+          this.playTracks(this.$config.playTracks, position);
+        }).catch((error) => {
+          console.error(error);
+        });
+      } else {
+        window.firstLoad = false;
+      }
+    });
   },
 };
 </script>
