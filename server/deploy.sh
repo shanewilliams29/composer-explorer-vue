cad263d6425909d47fa865561e07872f904814b3
Shane <shanewilliams29@gmail.com>
2024-06-16 12:09:19 -0700

    Improve deployment script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,62 +1,51 @@
- #!/bin/bash
+#!/bin/bash
 
-# activate venv
-source venv/bin/activate
+# Activate virtual environment
+echo "Activating virtual environment..."
+source venv/bin/activate || { echo -e "\033[31mFailed to activate virtual environment. Exiting.\033[0m"; exit 1; }
 
- # Save the current directory
+# Save the current directory
 initial_dir=$(pwd)
 
 # Run unit tests
 echo -e "\nRunning unit tests..."
 python3 -m unittest tests.py
-
-# Check if tests were successful
 if [ $? -eq 0 ]; then
-  echo -e "\033[32m\nUnit tests passed. Proceeding to deployment...\n\033[0m"
-
-  # Change directory to ../client and run npm run serve
-  cd ../client
-  echo "Running 'npm run build' in ../client directory..."
-  npm run build
-
-  # Check if npm run build was successful
-  if [ $? -eq 0 ]; then
-    echo -e "\033[32m'npm run build' successful. Project ready for deployment!\n\033[0m"
-  else
-    echo -e "\033[31m'npm run build' failed. Exiting.\n\033[0m"
-    exit 1
-  fi
+  echo -e "\033[32m\nUnit tests passed. Proceeding to deployment...\033[0m"
 else
-  echo -e "\033[31m\nUnit tests failed. Deployment canceled.\n\033[0m"
+  echo -e "\033[31m\nUnit tests failed. Deployment canceled.\033[0m"
   exit 1
 fi
 
-cd /home/shane/Documents/production/composer-explorer-vue/server/
+# Change directory to ../client and run npm run serve
+cd ../client || { echo -e "\033[31mFailed to change directory to ../client. Exiting.\033[0m"; exit 1; }
+echo "Running 'npm run build' in ../client directory..."
+npm run build
+if [ $? -eq 0 ]; then
+  echo -e "\033[32m'npm run build' successful. Project ready for deployment!\033[0m"
+else
+  echo -e "\033[31m'npm run build' failed. Exiting.\033[0m"
+  exit 1
+fi
 
-echo -e "Pulling from git for production server..."
+# Change directory to server folder in production
+cd /home/shane/Documents/production/composer-explorer-vue/server/ || { echo -e "\033[31mFailed to navigate to production server directory. Exiting.\033[0m"; exit 1; }
 
-git pull origin main
+# Pull latest updates from git
+echo -e "\nPulling from git for production server..."
+git pull origin main || { echo -e "\033[31mFailed to pull from Git. Exiting.\033[0m"; exit 1; }
 
-echo -e "Restarting gunicon..."
+# Copy static dist files to production server
+echo -e "\nCopying static dist files to production server..."
+rm -r /home/shane/Documents/production/composer-explorer-vue/server/dist || { echo -e "\033[31mFailed to remove old dist folder. Exiting.\033[0m"; exit 1; }
+cp -r /home/shane/Documents/composer-explorer-vue/server/dist /home/shane/Documents/production/composer-explorer-vue/server/dist || { echo -e "\033[31mFailed to copy dist files. Exiting.\033[0m"; exit 1; }
 
-sudo supervisorctl reload
+# Restart gunicorn using supervisor
+echo -e "\nRestarting gunicorn..."
+sudo supervisorctl reload || { echo -e "\033[31mFailed to restart gunicorn. Exiting.\033[0m"; exit 1; }
 
-echo -e "\033[32m'\nDeployment complete!\n\033[0m"
+# Print completion message
+echo -e "\033[32m\nDEPLOYMENT COMPLETE!\033[0m"
 
-
-
-# For Google App Engine Deployment
-#   # Change back to the initial directory (../server)
-#   cd "$initial_dir"
-  
-#   # Deploy to Google Cloud
-#   gcloud config set project composer-explorer
-#   gcloud app deploy --quiet
-
-#   # Check if deployment was successful
-#   if [ $? -eq 0 ]; then
-#     echo -e "\033[32m\nDeployment successful!\n\033[0m"
-#   else
-#     echo -e "\033[31m\nDeployment failed!\n\033[0m"
-#     exit 1
-#   fi
+# Return to the initial directory
+cd "$initial_dir"
